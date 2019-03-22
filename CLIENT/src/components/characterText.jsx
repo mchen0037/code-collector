6943514cc3bcb3740e2d53b21a0aad92470959b3
jsanchez254 <jsanchez254@ucmerced.edu>
2019-03-21 22:14:17 -0700

    added text file. Now opens on port 80

--- CLIENT/src/components/characterText.jsx
+++ CLIENT/src/components/characterText.jsx
@@ -12,9 +12,9 @@ class characterText extends Component {
         return (
             <React.Fragment>
                 <div className = "characterText">
-                    <div className = "popup" id = "textPosition">
-                        <div className = "popuptext" id = "myPopup">To turn the red light on for 5 seconds, type:<br/><br/><pre>red.on()<br/>wait(5)</pre><br/>and press the Play button</div>
-                    </div>
+                <div className = "popup" id = "textPosition">
+                    <div className = "popuptext" id = "myPopup">HELLO THERE!!!</div>    <br/>
+                </div>
                     <center><img alt = "" onClick = {()=> popOut()} onClick = {()=> sendText()} className = "characterTalks" src = {JIJI}/></center>
                 </div>
             </React.Fragment>
